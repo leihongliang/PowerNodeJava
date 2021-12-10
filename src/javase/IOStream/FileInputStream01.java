4a68bcf5b602f2956f222b20be7e3a50fb636bc0
leihongliang <1204062841@qq.com>
2021-12-10 22:02:51 +0800

    Initial commit

--- src/javase/IOStream/FileInputStream01.java
+++ src/javase/IOStream/FileInputStream01.java
@@ -1,4 +1,14 @@
 package javase.IOStream;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class FileInputStream01 {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("E:\\HDU\\Java\\Learn\\exe\\iostreamtest.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
